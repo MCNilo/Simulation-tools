188430d1c159d35f9d4facbb8c3cd1a738f7a820
MCNilo <nilomata02@gmail.com>
2026-04-24 12:04:55 +0200

    Update LGB.m

--- LGB.m
+++ LGB.m
@@ -6,7 +6,6 @@ zr=0.5*k*w0.^2;%Rayleigh range
 uxs=sqrt(2*factorial(p)./(pi*factorial(abs(m)+p)));%normalizing constant
 w=w0*sqrt(1 + (Z/zr).^2);%width of the gaussian
 La=Laguerre(p,abs(m),2*r.^2./(w.^2));%Laguerre polinomial
-% R=Z.*(1 + (zr./Z).^2);%curvature radius
 R1 = Z./(Z.^2+zr^2);%inverse of curvature radius
 Psi=(abs(m) + 2*p + 1).*atan(Z./zr);%Gouy phase
 outoam=uxs*(1./w).*((sqrt(2)*r)./w).^abs(m).*exp(-(r.^2./w.^2)).*...
