3c54bbd2f86f573dea5da1b93e14da5d77df380b
MCNilo <nilomata02@gmail.com>
2026-04-24 12:17:01 +0200

    Create LGB.m

---
+++ Codes/LGB.m
@@ -0,0 +1,14 @@
+%% Returns the Laguerre-Gaussian beam with wavelength "lambda", beam waist "w0", at positions X,Y,Z, azimuthal and radial orders "m" and "p"
+function outoam=LGB(lambda,w0,x,y,Z,m,p)
+[phi,r]=cart2pol(x,y);
+k=2*pi/lambda;
+zr=0.5*k*w0.^2;%Rayleigh range
+uxs=sqrt(2*factorial(p)./(pi*factorial(abs(m)+p)));%normalizing constant
+w=w0*sqrt(1 + (Z/zr).^2);%width of the gaussian
+La=Laguerre(p,abs(m),2*r.^2./(w.^2));%Laguerre polinomial
+R1 = Z./(Z.^2+zr^2);%inverse of curvature radius
+Psi=(abs(m) + 2*p + 1).*atan(Z./zr);%Gouy phase
+outoam=uxs*(1./w).*((sqrt(2)*r)./w).^abs(m).*exp(-(r.^2./w.^2)).*...
+exp(1i*k*Z).*La.*exp((1i)*k*(r.^2.*R1./2)).*exp(1i*m*phi).*exp(-1i*Psi);
+
+end
